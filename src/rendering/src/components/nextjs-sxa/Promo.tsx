a6077ca6703b72f18bc63010e4772fa93964f635
Neli Kostadinova <nko@sitecore.net>
2022-08-29 14:33:25 +0300

    Promo redesign

--- src/rendering/src/components/nextjs-sxa/Promo.tsx
+++ src/rendering/src/components/nextjs-sxa/Promo.tsx
@@ -40,8 +40,14 @@ export const Default = (props: PromoProps): JSX.Element => {
             <JssImage field={props.fields.PromoIcon} />
           </div>
           <div className="promo-text">
+            <div className="field-promotext3">
+              <Text className="promo-text" field={props.fields.PromoText3} />
+            </div>
             <div className="field-promotext">
-              <JssRichText field={props.fields.PromoText} />
+              <Text className="promo-text" field={props.fields.PromoText} />
+            </div>
+            <div className="field-promotext2">
+              <Text className="promo-text" field={props.fields.PromoText2} />
             </div>
             <div className="field-promolink">
               <JssLink field={props.fields.PromoLink} className="btn--main btn--main--round" />
@@ -55,47 +61,28 @@ export const Default = (props: PromoProps): JSX.Element => {
   return <PromoDefaultComponent {...props} />;
 };
 
-export const WithText = (props: PromoProps): JSX.Element => {
+export const WithColumns = (props: PromoProps): JSX.Element => {
   if (props.fields) {
     return (
-      <div className={`component promo ${props.params.styles}`}>
+      <div className={`component promo promo-columns ${props.params.styles}`}>
         <div className="component-content">
           <div className="field-promoicon">
             <JssImage field={props.fields.PromoIcon} />
           </div>
           <div className="promo-text">
-            <div className="field-promotext">
-              <Text className="promo-text" field={props.fields.PromoText} />
-            </div>
-            <div className="field-promotext2">
-              <Text className="promo-text" field={props.fields.PromoText2} />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  return <PromoDefaultComponent {...props} />;
-};
-
-export const WithSubtitle = (props: PromoProps): JSX.Element => {
-  if (props.fields) {
-    return (
-      <div className={`component promo ${props.params.styles}`}>
-        <div className="component-content">
-          <div className="field-promoicon">
-            <JssImage field={props.fields.PromoIcon} />
-          </div>
-          <div className="promo-text">
-            <div className="field-promotext3">
-              <Text className="promo-text" field={props.fields.PromoText3} />
-            </div>
-            <div className="field-promotext">
-              <Text className="promo-text" field={props.fields.PromoText} />
-            </div>
-            <div className="field-promotext2">
-              <Text className="promo-text" field={props.fields.PromoText2} />
+            <div className="text-cols">
+              <div className="field-promotext">
+                <JssImage field={props.fields.PromoIcon2} />
+                <JssRichText className="rich-text" field={props.fields.PromoText} />
+              </div>
+              <div className="field-promotext2">
+                <JssImage field={props.fields.PromoIcon2} />
+                <JssRichText className="rich-text" field={props.fields.PromoText2} />
+              </div>
+              <div className="field-promotext3">
+                <JssImage field={props.fields.PromoIcon2} />
+                <JssRichText className="rich-text" field={props.fields.PromoText3} />
+              </div>
             </div>
             <div className="field-promolink">
               <JssLink field={props.fields.PromoLink} className="btn--main btn--main--round" />
