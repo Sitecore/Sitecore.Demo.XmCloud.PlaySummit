f3cf56c67227c393997687573b071bb7c2ac1c56
Alexander Doroshenko <adoprog@users.noreply.github.com>
2022-10-13 14:04:53 -0400

    Update up.ps1

--- up.ps1
+++ up.ps1
@@ -142,6 +142,7 @@ if ($LASTEXITCODE -ne 0) {
     Write-Error "Serialization push failed, see errors above."
 }
 
+# DEMO TEAM CUSTOMIZATION - Restart CM after deserialization to clear the caches
 Invoke-WebRequest https://$xmCloudHost/Utilities/Restart.aspx
 
 # DEMO TEAM CUSTOMIZATION - Moved index rebuild here.
