7305a54a73f5387972175ae648bc804f437a5fb3
Jeff L'Heureux <jel@sitecore.net>
2022-11-01 09:15:28 -0400

    Re-introduce our custom CDP/Personalize integration

--- src/rendering/src/pages/api/cdp/config.ts
+++ src/rendering/src/pages/api/cdp/config.ts
@@ -1,6 +1,6 @@
-const clientKey = process.env.NEXT_PUBLIC_CDP_CLIENT_KEY;
-const apiTargetEndpoint = process.env.CDP_AUTHENTICATED_API_TARGET_ENDPOINT;
-const apiToken = process.env.CDP_API_TOKEN;
+const clientKey = process.env.NEXT_PUBLIC_CUSTOM_CDP_CLIENT_KEY;
+const apiTargetEndpoint = process.env.CUSTOM_CDP_AUTHENTICATED_API_TARGET_ENDPOINT;
+const apiToken = process.env.CUSTOM_CDP_API_TOKEN;
 
 export const config = {
   clientKey,
