a2f3ea0a8b78fcfa9053b7d97127840f92baa74b
Alexander Doroshenko <adoprog@users.noreply.github.com>
2023-03-28 07:26:23 -0400

    Workaround / hotfix for fallback issues (#67)

---
+++ src/platform/Utilities/Hotfix.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Sitecore.Configuration;
+
+namespace Sitecore.Demo.Edge.Website.Utilities
+{
+	public partial class Hotfix : System.Web.UI.Page
+	{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                var item = Factory.GetDatabase("master").GetItem("/sitecore/system/Languages/fr-CA");
+                item.Editing.BeginEdit();
+                item[FieldIDs.Revision] = Guid.NewGuid().ToString("D", CultureInfo.InvariantCulture);
+                item.Editing.EndEdit();
+            }
+        }
+    }
+}
