a8b6cb8e0cb2ae05a5519df357a7659d39889833
Alexander Doroshenko <adoprog@users.noreply.github.com>
2023-04-25 12:14:59 -0400

    AB#8712 Update Components references in items on instance startup

---
+++ src/platform/Utilities/Components.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Spe.Core.Host;
+
+namespace Sitecore.Demo.Edge.Website.Utilities
+{
+	public partial class Components : System.Web.UI.Page
+	{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                using (ScriptSession scriptSession = ScriptSessionManager.NewSession("Default", true))
+                {
+                    var libraryId = Request.QueryString["libraryId"];
+                    var speScriptItem = Sitecore.Context.Database.GetItem("/sitecore/system/Modules/PowerShell/Script Library/PLAY/Update Components LibraryId");
+                    var script = speScriptItem["Script"];
+
+                    if (!string.IsNullOrEmpty(script) && !string.IsNullOrEmpty(libraryId))
+                    {
+                        script = script.Replace("{{libraryId}}", libraryId);
+                        scriptSession.ExecuteScriptPart(script, true);
+                        Response.Write("Success!");
+                    }
+                    else
+                    {
+                        Response.Write("Make sure all required parameters are provided.");
+                    }
+                }
+            }
+        }
+    }
+}
