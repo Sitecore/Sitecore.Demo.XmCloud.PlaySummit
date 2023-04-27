7dd27184eeb051333f2acfbda6982d461e2abb71
Alexander Doroshenko <adoprog@users.noreply.github.com>
2023-04-27 15:07:28 -0400

    AB#8712 Update Components references in items on instance startup (#70)

---
+++ src/platform/Utilities/InitializeComponentsLibraryId.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Spe.Core.Host;
+
+namespace Sitecore.Demo.Edge.Website.Utilities
+{
+	public partial class InitializeComponentsLibraryId : System.Web.UI.Page
+	{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                using (ScriptSession scriptSession = ScriptSessionManager.NewSession("Default", true))
+                {
+                    var libraryId = Environment.GetEnvironmentVariable("XMCLOUD_PROJECT_ID");
+                    var speScriptItem = Sitecore.Context.Database.GetItem("/sitecore/system/Modules/PowerShell/Script Library/PLAY/Update Components LibraryId");
+                    var script = speScriptItem?["Script"];
+
+                    if (!string.IsNullOrEmpty(script) && !string.IsNullOrEmpty(libraryId))
+                    {
+                        script = script.Replace("{{libraryId}}", libraryId);
+                        scriptSession.ExecuteScriptPart(script, true);
+                        Response.Write("Success!");
+                    }
+                    else
+                    {
+                        Response.Write("Make sure all required parameters are provided.");
+                    }
+                }
+            }
+        }
+    }
+}
