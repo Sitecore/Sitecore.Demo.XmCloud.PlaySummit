113f4a2f4bc7a1f5919e01f2d705f21d22edf2e0
Rob Horton <rob.horton@sitecore.com>
2023-04-28 12:10:08 +1000

    Update HeroImage and HeaderBanner to use JSS Image
    Allows the images to be selected from the Media Library / CH in Pages

--- src/rendering/src/components/PageContent/HeaderBanner.tsx
+++ src/rendering/src/components/PageContent/HeaderBanner.tsx
@@ -1,4 +1,10 @@
-import { Text, Field, ImageField, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  Text,
+  Field,
+  ImageField,
+  withDatasourceCheck,
+  Image as JssImage,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 
 export type HeaderBannerProps = ComponentProps & {
@@ -11,14 +17,9 @@ export type HeaderBannerProps = ComponentProps & {
 };
 
 const HeaderBanner = (props: HeaderBannerProps): JSX.Element => {
-  const sectionStyles = props.fields.backgroundImage?.value?.src
-    ? {
-        backgroundImage: `url("${props.fields.backgroundImage.value.src}")`,
-      }
-    : {};
-
   return (
-    <section className="section header-banner" style={sectionStyles}>
+    <section className="section header-banner">
+      <JssImage field={props.fields.backgroundImage} />
       <div className="section-content section-content-left container">
         <Text className="eyebrow" tag="p" field={props.fields.eyebrow} />
         <Text className="title" tag="h1" field={props.fields.title} />
