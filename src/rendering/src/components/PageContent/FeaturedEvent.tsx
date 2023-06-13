52792038eb586403dc8def17397970bffbf54a09
Neli Kostadinova <nko@sitecore.net>
2023-06-13 08:55:25 +0300

    FeaturedEvent is fully editable

--- src/rendering/src/components/PageContent/FeaturedEvent.tsx
+++ src/rendering/src/components/PageContent/FeaturedEvent.tsx
@@ -1,18 +1,44 @@
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Field, Image, ImageField, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 import Link from 'next/link';
+import { Speaker } from 'src/types/speaker';
 
-const FeaturedEvent = (props: ComponentProps): JSX.Element => {
+export type FeaturedEventProps = ComponentProps & {
+  fields: {
+    Name: Field<string>;
+    Image: ImageField;
+    Premium: Field<boolean>;
+    Speakers: Speaker[];
+  };
+};
+
+const FeaturedEvent = (props: FeaturedEventProps): JSX.Element => {
   const sxaStyles = `${props.params?.styles || ''}`;
+  console.log(props);
+
+  const speakersSection = props.fields?.Speakers.map((speaker) => (
+    <div className="event-subtitle" key={speaker.fields.Name.value}>
+      <FontAwesomeIcon className="icon" icon={faUser} />
+      <Text field={speaker.fields?.Name} />
+      <Text field={speaker.fields?.JobTitle} tag="p" className="event-subtext" />
+    </div>
+  ));
 
   return (
-    <section className={`featured-event ${sxaStyles}`}>
+    <section
+      className={`featured-event ${
+        props.fields?.Premium.value ? 'premium-event' : ''
+      } ${sxaStyles}`}
+    >
+      <Image field={props.fields?.Image} alt="Image" loading="lazy" />
       <div className="content">
         <div className="triangle-area">
           <div className="text-area">
             <h1 className="section-title">Featured Event</h1>
-            <h3 className="event-title">Guest Keynote</h3>
-            <p className="section-title">Chris Williams</p>
-            <p className="event-subtitle">Professional Cyclist</p>
+            <Text field={props.fields?.Name} className="event-title" tag="h1" />
+            {speakersSection}
           </div>
           <div className="btn-area">
             <Link href="/tickets" className="btn-main">
