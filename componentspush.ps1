c580811c517fa39592234252ff445a5002362d04
Alexander Doroshenko <adoprog@users.noreply.github.com>
2023-06-27 16:03:57 -0400

    Update componentspush.ps1

--- componentspush.ps1
+++ componentspush.ps1
@@ -6,31 +6,30 @@
 )
 
 $headers = @{
-    "x-api-key"=$apiKey
+  "x-api-key" = $apiKey
 }
 
 # Components
 $collections = Get-Content "$rootFolder\collections.json" | ConvertFrom-Json
-foreach ($collection in $collections)
-{
-    Write-Host "Collection: " $collection.Name
+foreach ($collection in $collections) {
+  Write-Host "Collection: " $collection.
+  Invoke-WebRequest -Uri "$endpoint/libraries/$libraryId/collections" -Method Post -Body ($collection | ConvertTo-Json -depth 100) -Headers $headers -ContentType "application/json"
 
-   foreach ($component in $collection.components)
-   {
-      Write-Host "  Component: " $component.Name
+  foreach ($component in $collection.components) {
+    Write-Host "  Component: " $component.Name
 
-      $componentUrl = "$endpoint/libraries/$libraryId/collections/$($collection.id)/components"
-      Invoke-WebRequest -Uri $componentUrl -Method Post -Body ($component | ConvertTo-Json -depth 100) -Headers $headers -ContentType "application/json"
+    $componentUrl = "$endpoint/libraries/$libraryId/collections/$($collection.id)/components"
+    Invoke-WebRequest -Uri $componentUrl -Method Post -Body ($component | ConvertTo-Json -depth 100) -Headers $headers -ContentType "application/json"
 
-      $versions = Get-Content "$rootFolder\components\$($component.id).json" | ConvertFrom-Json
+    $versions = Get-Content "$rootFolder\components\$($component.id).json" | ConvertFrom-Json
 
-      foreach($version in $versions){
-        Write-Host "    Version: " $version.Name "(" $version.status ")"
+    foreach ($version in $versions) {
+      Write-Host "    Version: " $version.Name "(" $version.status ")"
 
-        $versionsUrl = "$endpoint/libraries/$libraryId/collections/$($collection.id)/components/$($component.id)/versions"
-        Invoke-WebRequest -Uri $versionsUrl -Method Post -Body ($version | ConvertTo-Json -depth 100) -Headers $headers -ContentType "application/json"
-      }
-   }
+      $versionsUrl = "$endpoint/libraries/$libraryId/collections/$($collection.id)/components/$($component.id)/versions"
+      Invoke-WebRequest -Uri $versionsUrl -Method Post -Body ($version | ConvertTo-Json -depth 100) -Headers $headers -ContentType "application/json"
+    }
+  }
 }
 
 # Styles
@@ -44,8 +43,7 @@ Invoke-WebRequest -Uri $stylesUrl -Method Put -Body ($styles | ConvertTo-Json -d
 $datasourcesUrl = "$endpoint/libraries/$libraryId/datasources"
 $datasources = Get-Content "$rootFolder\datasources.json" | ConvertFrom-Json
 
-foreach ($datasource in $datasources)
-{
+foreach ($datasource in $datasources) {
   Write-Host "Datasource: " $datasource.Name
   Invoke-WebRequest -Uri $datasourcesUrl -Method Post -Body ($datasource | ConvertTo-Json -depth 100) -Headers $headers -ContentType "application/json"
 }
