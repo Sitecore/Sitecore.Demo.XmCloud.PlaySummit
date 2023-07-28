5eaa199d4e795da00d4e50dde7f654a97c925dcc
Alexander Doroshenko <adoprog@users.noreply.github.com>
2023-07-28 09:40:21 -0400

    NuGet fixes and Discover upgrade (#96)

--- src/rendering/src/stories/Navigation/ShopNavigation.stories.tsx
+++ src/rendering/src/stories/Navigation/ShopNavigation.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Meta, StoryFn } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import ShopNavigation from '../../components/Navigation/ShopNavigation';
-import { initialize as initializeDiscover } from '../../services/DiscoverService';
+import { DiscoverService } from '../../services/DiscoverService';
 import { mockDiscoverData } from '../mock-discover-data';
 import { MockStore } from '../mock-store';
 import { cartSlice, loggedInAuthSlice } from '../Checkout/CheckoutCommon';
@@ -10,22 +10,20 @@ import { cartSlice, loggedInAuthSlice } from '../Checkout/CheckoutCommon';
 export default {
   title: 'Components/Navigation/ShopNavigation',
   component: ShopNavigation,
-} as Meta<typeof ShopNavigation>;
+} as ComponentMeta<typeof ShopNavigation>;
 
-initializeDiscover({
-  isStorybook: true,
-});
+const Template: ComponentStory<typeof ShopNavigation> = (args) => <ShopNavigation {...args} />;
 
-export const Default = {
-  args: {
-    previewSearchProps: mockDiscoverData.previewSearchProps,
-  },
+DiscoverService();
 
-  decorators: [
-    (Story: StoryFn) => (
-      <MockStore sliceOrSlices={[cartSlice, loggedInAuthSlice]}>
-        <Story />
-      </MockStore>
-    ),
-  ],
+export const Default = Template.bind({});
+Default.args = {
+  previewSearchProps: mockDiscoverData.previewSearchProps,
 };
+Default.decorators = [
+  (Story) => (
+    <MockStore sliceOrSlices={[cartSlice, loggedInAuthSlice]}>
+      <Story />
+    </MockStore>
+  ),
+];
