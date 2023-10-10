81df0ca76de5495e6010341904fe810d66b44513
Alexander Doroshenko <adoprog@users.noreply.github.com>
2023-10-10 08:22:37 -0400

    #8983 Generate page screenshots on startup

---
+++ src/platform/Utilities/GeneratePageScreenshots.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Spe.Core.Host;
+
+namespace Sitecore.Demo.Edge.Website.Utilities
+{
+	public partial class GeneratePageScreenshots : System.Web.UI.Page
+	{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                using (ScriptSession scriptSession = ScriptSessionManager.NewSession("Default", true))
+                {
+                    var speScriptItem = Sitecore.Context.Database.GetItem(
+                        "/sitecore/system/Modules/PowerShell/Script Library/PLAY/Generate Page Screenshots");
+                    var script = speScriptItem?["Script"];
+
+                    scriptSession.ExecuteScriptPart(script, true);
+                    Response.Write("Success!");
+                }
+            }
+        }
+    }
+}
