888947301a5c2d16b66058fb3573f5f84092f171
Christos Papoutsakis <chpa@sitecore.net>
2024-01-08 17:05:03 +0200

    Update some preview search related files

--- src/rendering/src/components/PreviewSearchContent/PreviewSearchPopup.tsx
+++ src/rendering/src/components/PreviewSearchContent/PreviewSearchPopup.tsx
@@ -1,81 +1,57 @@
-import { forwardRef } from 'react';
-import { ContentSearchNews } from '../../interfaces/contentSearch/ContentSearchNews';
-import { ContentSearchWidgetResponse } from '../../interfaces/contentSearch/ContentSearchWidgetResponse';
-import { ContentSearchSession } from '../../interfaces/contentSearch/ContentSearchSession';
-import { ContentSearchSpeaker } from '../../interfaces/contentSearch/ContentSearchSpeaker';
 import PreviewSearchNewsList from './PreviewSearchNewsList';
 import PreviewSearchSessionList from './PreviewSearchSessionList';
 import PreviewSearchSpeakerList from './PreviewSearchSpeakerList';
 import SuggestionList from './SuggestionList';
+import { SearchResponseSuggestion } from '@sitecore-search/react';
+import { NewsCardProps } from './NewsCard';
+import { SpeakerCardProps } from './SpeakerCard';
+import { SessionCardProps } from './SessionCard';
 
 type PreviewSearchPopupProps = {
   resultsUrl: string;
-  news: ContentSearchWidgetResponse<ContentSearchNews>;
-  sessions: ContentSearchWidgetResponse<ContentSearchSession>;
-  speakers: ContentSearchWidgetResponse<ContentSearchSpeaker>;
+  sessions: unknown[];
+  speakers: unknown[];
+  news: unknown[];
+  suggestions: SearchResponseSuggestion[];
   close?: () => void;
+  widgetRef: (node: Element) => void;
 };
 
-type Suggestion = {
-  text: string;
-  freq: number;
-};
-
-type SuggestionMap = {
-  [key: string]: Array<Suggestion>;
-};
-
-const getSuggestions = (
-  newsSuggestions: SuggestionMap,
-  sessionsSuggestions: SuggestionMap,
-  speakersSuggestions: SuggestionMap
-) => [
-  ...((newsSuggestions && newsSuggestions['content_name_context_aware']) || []),
-  ...((sessionsSuggestions && sessionsSuggestions['session_name_context_aware']) || []),
-  ...((speakersSuggestions && speakersSuggestions['speaker_name_context_aware']) || []),
-];
-
-const PreviewSearchPopup = forwardRef<HTMLDivElement, PreviewSearchPopupProps>(
-  (
-    { resultsUrl, news, sessions, speakers }: PreviewSearchPopupProps,
-    forwardedRef
-  ): JSX.Element => {
-    const suggestions = getSuggestions(
-      news?.suggestion as unknown as SuggestionMap,
-      sessions?.suggestion as unknown as SuggestionMap,
-      speakers?.suggestion as unknown as SuggestionMap
-    );
-    const sessionsAvailable = sessions && sessions.total_item > 0;
-    const speakersAvailable = speakers && speakers.total_item > 0;
-    const newsAvailable = news && news.total_item > 0;
-
-    return (
-      ((suggestions && suggestions.length > 0) ||
-        sessionsAvailable ||
-        speakersAvailable ||
-        newsAvailable) && (
-        <div className="preview-search-content-container" ref={forwardedRef}>
-          <div className="preview-search-content">
-            <div className="preview-search-content-popup">
-              {suggestions && suggestions.length > 0 && (
-                <SuggestionList title="Do you mean?" list={suggestions} />
-              )}
-              {sessionsAvailable && (
-                <PreviewSearchSessionList resultsUrl={resultsUrl} list={sessions.content} />
-              )}
-              {speakersAvailable && (
-                <PreviewSearchSpeakerList resultsUrl={resultsUrl} list={speakers.content} />
-              )}
-              {newsAvailable && (
-                <PreviewSearchNewsList resultsUrl={resultsUrl} list={news.content} />
-              )}
-            </div>
-          </div>
+const PreviewSearchPopup = ({
+  resultsUrl,
+  sessions,
+  speakers,
+  news,
+  suggestions,
+  widgetRef,
+}: PreviewSearchPopupProps): JSX.Element => {
+  return (
+    <div className="preview-search-content-container" ref={widgetRef}>
+      <div className="preview-search-content">
+        <div className="preview-search-content-popup">
+          {suggestions?.length > 0 && <SuggestionList title="Do you mean?" list={suggestions} />}
+          {sessions?.length > 0 && (
+            <PreviewSearchSessionList
+              resultsUrl={resultsUrl}
+              list={sessions as unknown as SessionCardProps[]}
+            />
+          )}
+          {speakers?.length > 0 && (
+            <PreviewSearchSpeakerList
+              resultsUrl={resultsUrl}
+              list={speakers as unknown as SpeakerCardProps[]}
+            />
+          )}
+          {news?.length > 0 && (
+            <PreviewSearchNewsList
+              resultsUrl={resultsUrl}
+              list={news as unknown as NewsCardProps[]}
+            />
+          )}
         </div>
-      )
-    );
-  }
-);
-PreviewSearchPopup.displayName = 'PreviewSearchPopup';
+      </div>
+    </div>
+  );
+};
 
 export default PreviewSearchPopup;
