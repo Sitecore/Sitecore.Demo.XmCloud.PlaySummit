95c42141f252be42a873ed6d45863b0e974e3476
Christos Papoutsakis <chpa@sitecore.net>
2024-01-09 20:22:27 +0200

    Update PersonalizedPicksWrapper.tsx

--- src/rendering/src/components/ContentSearch/PersonalizedPicksWrapper.tsx
+++ src/rendering/src/components/ContentSearch/PersonalizedPicksWrapper.tsx
@@ -1,5 +1,13 @@
+import { isSearchSDKEnabled } from '../../services/SearchSDKService';
 import PersonalizedPicks from './PersonalizedPicks';
 
-const PersonalizedPicksWrapper = () => <PersonalizedPicks rfkId="rfkid_pp" />;
+const PersonalizedPicksWrapper = () =>
+  isSearchSDKEnabled ? (
+    <PersonalizedPicks rfkId="rfkid_pp" />
+  ) : (
+    <div className="personalized-picks">
+      <div className="container">Search integration is not configured</div>
+    </div>
+  );
 
 export default PersonalizedPicksWrapper;
