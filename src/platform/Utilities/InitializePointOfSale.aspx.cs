1c083dccca8c1f087db1e90b8d52947589af45ba
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-01-23 17:36:58 -0500

    Updates

---
+++ src/platform/Utilities/InitializePointOfSale.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sitecore.Demo.Edge.Website.Utilities
+{
+    public partial class InitializePointOfSale : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                var item = Sitecore.Context.Database.GetItem("/sitecore/content/PLAY/playwebsite/Settings/Site Grouping/playwebsite");
+
+                item.Editing.BeginEdit();
+                try
+                {
+                    item["POS"] = "*=playwebsite";
+                    item.Editing.EndEdit();
+                }
+                catch (Exception ex)
+                {
+                    item.Editing.CancelEdit();
+                }
+
+                Response.Write("Success!");
+               
+            }
+        }
+    }
+}
