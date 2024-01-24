325a728ef48e050bff6be1b1cb15e81f45031fb3
Christos Papoutsakis <96472119+chpa7@users.noreply.github.com>
2024-01-24 20:10:52 +0200

    AB#9142 Remove old/custom Sitecore Search code (#156)

--- src/rendering/src/interfaces/contentSearch/FacetValueClickedActionPayload.ts
+++
@@ -1,8 +0,0 @@
-export type FacetValueClickedActionPayload = {
-  facetId: string;
-  facetValueId: string;
-  facetIndex: number;
-  facetValueIndex: number;
-  checked: boolean;
-  type: 'text' | 'range' | 'valueId';
-};
