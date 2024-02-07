5d3a6429b18301bb0848a195f69afae07bb6167d
Christos Papoutsakis <chpa@sitecore.net>
2024-02-07 18:06:13 +0200

    Update OrderSummary.tsx

--- src/rendering/src/components/Payment/OrderSummary.tsx
+++ src/rendering/src/components/Payment/OrderSummary.tsx
@@ -1,25 +1,25 @@
+import { useEffect, useState } from 'react';
+
 import { TICKETS } from '../../models/mock-tickets';
 
 type OrderSummaryProps = {
-  ticket?: string;
+  ticketID?: number;
 };
 
-const OrderSummary = (props?: OrderSummaryProps): JSX.Element => {
-  let ticketId = '0';
-  if (props?.ticket) {
-    ticketId = props.ticket;
-  }
-  if (typeof window !== 'undefined') {
-    const queryStringTicket = new URLSearchParams(window.location.search).get('ticket');
-    if (queryStringTicket) {
-      ticketId = queryStringTicket;
-    }
-  }
-  const ticket = TICKETS[parseInt(ticketId)];
+const OrderSummary = (props: OrderSummaryProps): JSX.Element => {
+  const [ticketID, setTicketID] = useState(null);
+
+  useEffect(() => {
+    setTicketID(
+      props?.ticketID ?? parseInt(new URLSearchParams(window?.location?.search).get('ticket'))
+    );
+  }, [props?.ticketID]);
+
+  const ticket = TICKETS[ticketID];
 
   // The empty div in the else clause is required. Without it, the class="line-item total-line" of the total line is not rendered to the DOM for unknown reasons.
   const fees =
-    ticket.fees > 0 ? (
+    ticket?.fees > 0 ? (
       <div>
         <div className="line-item">
           <div className="item-name">Fees</div>
@@ -30,6 +30,8 @@ const OrderSummary = (props?: OrderSummaryProps): JSX.Element => {
     ) : (
       <div></div>
     );
+
+  if (!ticket) return <></>;
 
   return (
     <div className="order-summary">
