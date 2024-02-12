92a2d2420f097e94d8b1a3ba8950ee6165692a43
Christos Papoutsakis <chpa@sitecore.net>
2024-02-12 19:18:22 +0200

    Add SEARCH_PROFILE_DATA custom event to CloudSDKService.ts

--- src/rendering/src/services/CloudSDKService.ts
+++ src/rendering/src/services/CloudSDKService.ts
@@ -1,5 +1,6 @@
 import { EPResponse } from '@sitecore-cloudsdk/core';
 import { IdentityEventAttributesInput } from '@sitecore-cloudsdk/events/browser';
+import Cookies from 'js-cookie';
 
 import { context } from 'lib/context';
 import { TICKETS } from 'src/models/mock-tickets';
@@ -61,5 +62,51 @@ export const logTicketPurchase = async (ticketId: number): Promise<EPResponse> =
       currency,
     },
     { ticketId, ticketName: purchasedTicketItem.name, pricePaid: purchasedTicketItem.price }
+  );
+};
+
+export const logSearchProfileData = async (payload: {
+  entities: [
+    {
+      affinity: { audience: [{ value: string; score: number }] };
+      events: { views: [{ id: string }] };
+    }
+  ];
+}): Promise<EPResponse> => {
+  // Transform the affinity scores & page views into suitable forms for the guest data extension
+  const affinities = payload?.entities?.[0].affinity?.audience?.reduce(
+    (obj, item) => (
+      (obj[
+        item.value
+          .split(' ')
+          .map((x, index) => (index === 0 ? x.toLowerCase() : x))
+          .join('')
+      ] = Math.round((item.score + Number.EPSILON) * 100) / 100),
+      obj
+    ),
+    {} as Record<string, number>
+  );
+
+  const pageViews = payload?.entities?.[0].events?.views
+    ?.slice(0, 5)
+    .reduce(
+      (obj, item, index) => ((obj[`page${index + 1}`] = sessionStorage.getItem(item.id)), obj),
+      {} as Record<string, string>
+    );
+
+  const extensionData = {
+    uuid: Cookies.get('__rutma').split('.')[0],
+    ...affinities,
+    ...pageViews,
+  };
+
+  const Events = await context.getSDK('Events');
+  return Events?.event(
+    'SEARCH_PROFILE_DATA',
+    {
+      channel,
+      currency,
+    },
+    { ...extensionData }
   );
 };
