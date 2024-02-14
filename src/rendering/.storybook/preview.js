6d8048d5c1eae9d9dd98540c703c620de1d29a6e
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-02-14 13:14:22 -0800

    AB#9199 Storybook update and fixes (#163)

--- src/rendering/.storybook/preview.js
+++ src/rendering/.storybook/preview.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SitecoreContext, LayoutServicePageState } from '@sitecore-jss/sitecore-jss-nextjs';
 import { componentBuilder } from '../src/temp/componentBuilder';
-import "../src/assets/css/abstracts/mixins.css";
-import "../src/assets/css/main.css";
+import '../src/assets/css/abstracts/mixins.css';
+import '../src/assets/css/main.css';
+import { I18nProvider } from 'next-localization';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -15,16 +16,16 @@ export const parameters = {
   layout: 'fullscreen',
   breakpoints: {
     breakpointNames: {
-      'default': '0',
-      'sm': '640',
-      'md': '768',
-      'lg': '1024',
-      'xl': '1280',
+      default: '0',
+      sm: '640',
+      md: '768',
+      lg: '1024',
+      xl: '1280',
       '2xl': '1536',
     },
     debounceTimeout: 200,
   },
-}
+};
 
 export const mockLayoutData = {
   sitecore: {
@@ -64,10 +65,14 @@ export const mockLayoutData = {
 export const decorators = [
   (Story) => (
     <SitecoreContext
-      componentFactory={componentBuilder.getComponentFactory({ isEditing: mockLayoutData.sitecore.context.pageEditing })}
+      componentFactory={componentBuilder.getComponentFactory({
+        isEditing: mockLayoutData.sitecore.context.pageEditing,
+      })}
       layoutData={mockLayoutData}
     >
-      <Story />
+      <I18nProvider>
+        <Story />
+      </I18nProvider>
     </SitecoreContext>
   ),
 ];
