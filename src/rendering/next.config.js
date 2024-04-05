6c3031da793c5521562d6c38347fc5ddfa6a514b
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-04-05 07:14:27 -0700

    AB#9256 Upgrade to JSS 21.7 (#180)

--- src/rendering/next.config.js
+++ src/rendering/next.config.js
@@ -37,6 +37,24 @@ const nextConfig = {
   // Enable React Strict Mode
   reactStrictMode: true,
 
+  // use this configuration to ensure that only images from the whitelisted domains
+  // can be served from the Next.js Image Optimization API
+  // see https://nextjs.org/docs/app/api-reference/components/image#remotepatterns
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'edge*.**',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'feaas*.blob.core.windows.net',
+        port: '',
+      },
+    ]
+  },
+
   async rewrites() {
     // When in connected mode we want to proxy Sitecore paths off to Sitecore
     return [
--- src/rendering/src/Scripts.tsx
+++ src/rendering/src/Scripts.tsx
@@ -1,12 +1,14 @@
 // The BYOC bundle imports external (BYOC) components into the app and makes sure they are ready to be used
 import BYOC from 'src/byoc';
 import CdpPageView from 'components/nextjs-sxa/CdpPageView';
+import FEAASScripts from 'components/nextjs-sxa/FEAASScripts';
 
 const Scripts = (): JSX.Element => {
   return (
     <>
       <BYOC />
       <CdpPageView />
+      <FEAASScripts />
     </>
   );
 };
