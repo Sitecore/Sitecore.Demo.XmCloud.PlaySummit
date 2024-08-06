94b8df01fb7517bca7e81e5ea953b18e6b72d992
Mannkp <mannkpandya@gmail.com>
2024-08-06 21:06:58 +0530

    Main Navigation Completed. (header nav)

--- src/rendering/src/components/Navigation/MainNavigation.tsx
+++ src/rendering/src/components/Navigation/MainNavigation.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
+import NextImage from 'next/image';
 import { ImageField, Image } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
-import { isCommerceEnabled } from '../../helpers/CommerceHelper';
-import { useI18n } from 'next-localization';
-import PreviewSearchWidget from '../PreviewSearchContent/PreviewSearchContent';
-import { isSearchSDKEnabled } from '../../services/SearchSDKService';
+import search from '../../assets/img/search.svg';
+import call from '../../assets/img/call.png';
 
 export type MainNavigationProps = ComponentProps & {
   fields: {
@@ -42,41 +41,57 @@ export type MainNavigationProps = ComponentProps & {
 };
 
 const MainNavigation = (props: MainNavigationProps): JSX.Element => {
+  console.log(props.fields?.data?.links?.children?.results);
   const [navbarOpen, setNavbarOpen] = useState(false);
-  const { t } = useI18n();
 
   const sxaStyles = `${props.params?.styles || ''}`;
 
-  const shopLink = isCommerceEnabled && (
-    <li className="text-menu-item">
-      <Link href="/shop">Shop</Link>
-    </li>
-  );
-
   return (
     <nav className={`main-navigation ${sxaStyles}`}>
-      <div className="navigation-content">
-        <div className="controls-container container">
+      <div className="navigation-content flex items-center">
+        <div className="controls-container">
           <Link href="/" className="logo-link">
             <Image
               field={props.fields.data.item.headerLogo.jsonValue}
               alt={props.fields.data.item.headerLogo.alt}
             />
           </Link>
-          <button
-            className="items-toggle"
-            aria-label="open menu"
-            type="button"
-            onClick={() => setNavbarOpen(!navbarOpen)}
-          >
-            <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
+          <div className="flex justify-evenly items-center">
+            <div className={'text-menu-item px-3 block lg:hidden '}>
+              <Link
+                href={
+                  props.fields?.data?.links?.children?.results?.field?.jsonValue?.value?.href ?? '#'
+                }
+                prefetch={false}
+              >
+                <NextImage src={search} width={18} height={18} alt="search icon" />
+              </Link>
+            </div>
+            <div className={'text-menu-item px-3 block lg:hidden'}>
+              <Link
+                href={
+                  props.fields?.data?.links?.children?.results?.field?.jsonValue?.value?.href ?? '#'
+                }
+                prefetch={false}
+              >
+                <NextImage src={call} width={22} height={22} alt="call icon" />
+              </Link>
+            </div>
+            <button
+              className="items-toggle"
+              aria-label="open menu"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
         <div className={'items-container' + (navbarOpen ? ' opened' : ' closed')}>
           <ul className="container">
@@ -87,18 +102,28 @@ const MainNavigation = (props: MainNavigationProps): JSX.Element => {
                 </Link>
               </li>
             ))}
-            {shopLink}
-            <li className="button-menu-item">
-              <Link href="/tickets" className="btn-main">
-                {t('Book Tickets') || 'Book Tickets'}
+            {/* it is required to add below static links for search and call us in above mapping (and edit href from " fields?. " )*/}
+            <li className={'text-menu-item md:px-3 hidden lg:block'}>
+              <Link
+                href={
+                  props.fields?.data?.links?.children?.results?.field?.jsonValue?.value?.href ?? '#'
+                }
+                prefetch={false}
+              >
+                <NextImage src={search} width={18} height={18} alt="search icon" />
+              </Link>
+            </li>
+            <li className={'text-menu-item md:px-3 hidden lg:block'}>
+              <Link
+                href={
+                  props.fields?.data?.links?.children?.results?.field?.jsonValue?.value?.href ?? '#'
+                }
+                prefetch={false}
+              >
+                <NextImage src={call} width={22} height={22} alt="call icon" />
               </Link>
             </li>
           </ul>
-          {isSearchSDKEnabled && (
-            <div className="search-input-container">
-              <PreviewSearchWidget rfkId="rfkid_6" />
-            </div>
-          )}
         </div>
       </div>
     </nav>
