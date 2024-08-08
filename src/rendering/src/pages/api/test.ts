ce70d3c4792fca62f391841957127ed3532bae77
vikassinghv34 <vikassinghv34@gmail.com>
2024-08-08 18:51:41 +0530

    Email API testing Part-2

--- src/rendering/src/pages/api/test.ts
+++ src/rendering/src/pages/api/test.ts
@@ -8,9 +8,16 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Respon
   const data = req?.query?.data;
   console.log(req);
 
+  if (req) {
+    alert('req' + req);
+  }
+
   if (data) {
     console.log('Email data:', eval('(' + data + ')')?.email);
   }
 
   res.status(200).json({ message: req?.query?.data as string });
+  if (res) {
+    alert('res' + res);
+  }
 }
